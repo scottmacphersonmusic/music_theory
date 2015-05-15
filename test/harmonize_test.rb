8485c3d7ebcbb6c10eb3bcb161712762c7e0cc23
scott <scottmacphersonmusic@gmail.com>
2015-05-14 20:02:37 -0700

    Add test for length of samples

--- test/harmonize_test.rb
+++ test/harmonize_test.rb
@@ -4,13 +4,19 @@ class TestHarmonize < MiniTest::Test
   def setup
     samples_one = [0, 1, 0, -1]
     samples_two = [1, 0, -1, 0]
+    @samples_three = [-1, 0, 1, 0, -1]
 
-    @subject = MusicTheory::Harmonize.new(samples_one, samples_two)
+    @subject = MusicTheory::Harmonize.new(samples_one, samples_two, @samples_three)
   end
 
   def test_samples_not_clipping
     samples = @subject.samples
     max = samples.map {|s| s.abs }.max
     assert_equal 1.0, max
+  end
+
+  def test_duration_matches_longest_input
+    result_len = @subject.samples.length
+    assert_equal @samples_three.length, result_len
   end
 end
