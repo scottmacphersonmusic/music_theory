4a5806827706fe0d171340aa418b68bd1a101d80
scott <scottmacphersonmusic@gmail.com>
2015-05-14 20:42:06 -0700

    Harmonizer#samples does its own work

--- lib/music_theory/harmonize.rb
+++ lib/music_theory/harmonize.rb
@@ -3,21 +3,24 @@ require 'music_theory/output'
 module MusicTheory
   class Harmonize
     include MusicTheory::Output
-    attr_accessor :samples
 
     def initialize(*things_to_flatten)
-      max_sample_array_length = things_to_flatten.map(&:length).max
+      @things_to_flatten = things_to_flatten
+    end
+
+    def samples
+      max_sample_array_length = @things_to_flatten.map(&:length).max
       @samples = Array.new(max_sample_array_length, 0)
 
-      things_to_flatten.each do |group|
+      @things_to_flatten.each do |group|
         group.each_with_index do |value, i|
           @samples[i] +=  value
         end
       end
 
-       max = @samples.map {|s| s.abs }.max
-       multiplier = 1.0 / max
-       @samples.map!{ |s| multiplier * s }
+      max = @samples.map {|s| s.abs }.max
+      multiplier = 1.0 / max
+      @samples.map!{ |s| multiplier * s }
     end
   end
 end
