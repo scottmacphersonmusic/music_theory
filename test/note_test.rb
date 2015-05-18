a4caae5769350fbd794ca2123486c22ad32617d4
scott <scottmacphersonmusic@gmail.com>
2015-05-17 19:03:31 -0700

    Add test for samples

--- test/note_test.rb
+++ test/note_test.rb
@@ -5,13 +5,14 @@ class TestNote < MiniTest::Test
     @frequency   = 220.to_f
     @duration    = 2.0
     @sample_rate = 22050
+    @total_frames = (@duration * @sample_rate).to_i
     @cycles_per_frame = @frequency / @sample_rate
+    @sine_wave_cycle = 2 * Math::PI * @cycles_per_frame
     @note = MusicTheory::Note.new(frequency: @frequency, duration: @duration)
   end
 
   def test_total_frames
-    frames = (@duration * @sample_rate).to_i
-    assert_equal frames, @note.total_frames
+    assert_equal @total_frames, @note.total_frames
   end
 
   def test_cycles_per_frame
@@ -19,7 +20,16 @@ class TestNote < MiniTest::Test
   end
 
   def test_sine_wave_cycle
-    sine_wave_cycle = 2 * Math::PI * @cycles_per_frame
-    assert_equal sine_wave_cycle, @note.sine_wave_cycle
+    assert_equal @sine_wave_cycle, @note.sine_wave_cycle
+  end
+
+  def test_samples
+    phase = 0
+    samples = @total_frames.times.map do
+      sample = (Math.sin phase).to_f
+      phase += @sine_wave_cycle
+      sample
+    end
+    assert_equal samples, @note.samples
   end
 end
